1d8cea0989be1957d65b0506aa6ac7b742af54be
Jaime Castro <jecastro.ie@gmail.com>
2021-08-24 21:36:54 -0400

    Add trust to key in gpg

--- gpg.sh
+++ gpg.sh
@@ -7,7 +7,10 @@ echo 'use-agent' > $HOME/.gnupg/gpg.conf
 
 chmod 700 $HOME/.gnupg
 
-vared -p 'Enter path for gpg backup ' -c gpg_backup_path
-gpg --import $gpg_backup_path 
-# TODO(jaimecastro): Trust key
+vared -p 'Enter path for gpg key backup ' -c gpg_key_backup_path
+gpg --import $gpg_key_backup_path 
+
+vared -p 'Enter path for gpg trustdb backup' -c gpg_trustdb_backup_path
+rm $HOME/.gnupg/trustdb.gpg
+gpg --import-ownertrust < $gpg_trustdb_backup_path
 
